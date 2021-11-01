5a2dc4d37117f2330de980830abc43367a9ea378
Husseljo <husseljoo@gmail.com>
2021-11-01 11:30:15 +0200

    flights create, delete and update finished

--- backend/app.js
+++ backend/app.js
@@ -6,25 +6,30 @@ const app = express();
 const port = process.env.PORT || "8000";
 const flights = require("./routers/flights");
 const users = require("./routers/users");
+const bp = require("body-parser");
 
 //add routers here
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use("/flights", flights);
 app.use("/users", users);
 
 //database connection.
 mongoose
-  .connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log("MongoDB is now connected");
+    .connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        console.log("MongoDB is now connected");
 
-    // Start server once database connection is established
-    app.listen(port, () => {
-      console.log(`Listening to requests on http://localhost:${port}...`);
-    });
-  })
-  .catch((err) => console.log(err));
+        // Start server once database connection is established
+        app.listen(port, () => {
+            console.log(`Listening to requests on http://localhost:${port}...`);
+        });
+    })
+    .catch((err) => console.log(err));
 
 //just to make sure that gets working properly for now... (edit later)
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Home Page<h1>");
+    res.status(200).send("<h1>Home Page<h1>");
 });
--- backend/routers/flights.js
+++ backend/routers/flights.js
@@ -1,28 +1,97 @@
 const express = require("express");
 const router = express.Router();
 const Flight = require("../models/flightSchema");
+const bp = require("body-parser");
+const User = require("../models/userSchema"); //for confirmation purposes later dont know how to use session
 
-//should fetch all flights from DB and send json data
-router.get("/all-flights", (req, res) => {
-  res.send("<h1>All Flights are to be shown here</h1>");
+//Creating new flight object and saving it to the database.
+router.post("/create-flight", async (req, res) => {
+    try {
+        const { flight_number, trip_time, trip_date, seat_number, airport, price } =
+            req.body.flights;
+
+        const newFlight = new Flight({
+            flight_number: flight_number,
+            trip_time: trip_time,
+            trip_date: trip_date,
+            seat_number: seat_number,
+            airport: airport,
+            price: price,
+        });
+
+        await newFlight.save();
+        console.log("creating new flight is successful");
+        res.status(201).send({ success: true });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: "creating new flight is unsuccessful",
+            error: err,
+        });
+    }
 });
 
-//////Getting all flights from the database with isAdmin role valid
-//router.get('/view-all-flights', async (req, res) => {
-//    if (req.body.User.isAdmin == true) { // checking if it is admin (although not sure if it makes a difference lol)
-//        const allFlights = await flights.find();
-//        await res.send(allFlights);
-//        ;
-//    }
-//});
+//Delete a Flight from the database
+// admin checker to be added
+router.delete("/delete-flight/:flight_number", async (req, res) => {
+    try {
+        const { flight_number } = req.params;
+        await Flight.deleteOne({ flight_number: flight_number });
+        console.log(`deleting ${flight_number} is successful`);
+        res.status(201).send({ success: true });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: `deleting ${flight_number} is unsuccessful`,
+            error: err,
+        });
+    }
+});
 
-////Creating new flight object and saving it to the database.
-//router.post('/create-flight', async (req, res) => {
-//    if (req.body.User.isAdmin == true) { // checking user is admin
-//        const newFlight = await new flights(req.body.flights); //taking from flight from the req json object
-//        // save flight to the database.
-//        newFlight.save();
-//    }
-//});
+//UPDATE_FLIGHT
+// admin checker to be added
+router.put("/update-flight/:flight_number", async (req, res) => {
+    const { flight_number } = req.params;
+    try {
+        const updateFlight = req.body.flights;
+        const filter = { flight_number: flight_number };
+        const updated = await Flight.findOneAndUpdate(filter, updateFlight, {
+            rawResult: true,
+        });
+
+        //check if an update actually took place
+        if (updated.lastErrorObject.updatedExisting) {
+            console.log(`updating ${flight_number} is successful`);
+            res.status(201).send({ success: true });
+        } else {
+            res.status(400).send({
+                success: false,
+                message: `updating ${flight_number} is unsuccessful`,
+                error: err,
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: `updating ${flight_number} is unsuccessful`,
+            error: err,
+        });
+    }
+});
+
+//SEARCH_FLIGHT
+//admin checker to be added
+router.get("/search-flight", async (req, res) => {
+    const searchFlight = req.body;
+    if (req.body != null) {
+        const flight = await Flight.find(searchFlight.flights);
+        res.send(flight);
+    } else {
+        await res.send("not found");
+    }
+});
 
 module.exports = router;
