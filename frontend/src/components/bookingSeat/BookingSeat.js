af9a282e7639017367dc81dbaa38d32f6642c4d5
Husseljo <husseljoo@gmail.com>
2021-12-03 15:19:29 +0200

    Seatpicker page retrieves data and passes props to BookingSeat

--- frontend/src/components/bookingSeat/BookingSeat.js
+++ frontend/src/components/bookingSeat/BookingSeat.js
@@ -3,23 +3,9 @@ import SeatPicker from "react-seat-picker";
 import axios from "axios";
 import "./BookingSeat.css";
 import createRows from "./createRows";
+import { useHistory } from "react-router-dom";
 
-export default function BookingSeat({ cabinClass, requestedSeats }) {
-    const [seatData, setSeatData] = useState([
-        { seat_number: "Z99", reserved: true },
-    ]);
-    const [remainingSeats, setRemainingSeats] = useState(-1);
-    const [rows, setRows] = useState([[]]);
-
-    //this should be passed from booking in previous page
-    //first off we have to check whether requestedSeatsNumber<= remainingSeats to continue picking seats
-    requestedSeats = 4;
-    cabinClass = "business";
-
-    useEffect(() => {
-        getSeats("QWR-235");
-    }, []);
-
+export default function BookingSeat({ rows }) {
     const addSeatCallback = ({ row, number, id }, addCb) => {
         console.log("row", row);
         console.log("number", number);
@@ -32,155 +18,8 @@ export default function BookingSeat({ cabinClass, requestedSeats }) {
         console.log("id", id);
     };
 
-    const getSeats = async (flightNumber) => {
-        try {
-            const url = `http://localhost:8000/flights/search?flight_number=${flightNumber}`;
-            const response = await axios.get(url);
-            const seatFullData = response.data[0].seats[cabinClass];
-            setSeatData(seatFullData);
-            setRemainingSeats(response.data[0].remaining_seats[cabinClass]);
-            setRows(createRows(seatFullData));
-            // setAllRowsBro();
-            // setRows([
-            //     seatFullData.map(({ seat_number, reserved }) => {
-            //         const createdSeat = {
-            //             id: seat_number,
-            //             number: seat_number,
-            //             isReserved: reserved,
-            //         };
-            //         return createdSeat;
-            //     }),
-            // ]);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    // const rows = [
-    //     [
-    //         { id: "E0", number: "E0", isReserved: false },
-    //         { id: "E1", number: "E1", isReserved: false },
-    //         { id: "E2", number: "E2", isReserved: false },
-    //         { id: "E3", number: "E3", isReserved: false },
-    //         null,
-    //         null,
-    //         { id: "E4", number: "E4", isReserved: false },
-    //         { id: "E5", number: "E5", isReserved: false },
-    //         { id: "E6", number: "E6", isReserved: false },
-    //         { id: "E7", number: "E7", isReserved: false },
-    //     ],
-    //     [
-    //         { id: "E8", number: "E8", isReserved: false },
-    //         { id: "E9", number: "E9", isReserved: false },
-    //         { id: "E10", number: "E10", isReserved: false },
-    //         { id: "E11", number: "E11", isReserved: false },
-    //         null,
-    //         null,
-    //         { id: "E12", number: "E12", isReserved: false },
-    //         { id: "E13", number: "E13", isReserved: false },
-    //         { id: "E14", number: "E14", isReserved: false },
-    //         { id: "E15", number: "E15", isReserved: false },
-    //     ],
-    //     [
-    //         { id: "E16", number: "E16", isReserved: false },
-    //         { id: "E17", number: "E17", isReserved: false },
-    //         { id: "E18", number: "E18", isReserved: false },
-    //         { id: "E19", number: "E19", isReserved: false },
-    //         null,
-    //         null,
-    //         { id: "E20", number: "E20", isReserved: false },
-    //         { id: "E21", number: "E21", isReserved: false },
-    //         { id: "E22", number: "E22", isReserved: false },
-    //         { id: "E23", number: "E23", isReserved: false },
-    //     ],
-    //     [
-    //         { id: "E24", number: "E24", isReserved: false },
-    //         { id: "E25", number: "E25", isReserved: false },
-    //         { id: "E26", number: "E26", isReserved: false },
-    //         { id: "E27", number: "E27", isReserved: false },
-    //         null,
-    //         null,
-    //         { id: "E28", number: "E28", isReserved: false },
-    //         { id: "E29", number: "E29", isReserved: false },
-    //         { id: "x", number: "x", isReserved: false },
-    //         { id: "x", number: "x", isReserved: false },
-    //     ],
-    // ];
-
-    // const rows = [
-    //     [
-    //         { id: 1, number: 1, isSelected: true, tooltip: "Reserved by you" },
-    //         { id: 2, number: 2, tooltip: "Cost: 15$" },
-    //         null,
-    //         {
-    //             id: 3,
-    //             number: "3",
-    //             isReserved: true,
-    //             orientation: "east",
-    //             tooltip: "Reserved by Rogger",
-    //         },
-    //         { id: 4, number: "4", orientation: "west" },
-    //         null,
-    //         { id: 5, number: 5 },
-    //         { id: 6, number: 6 },
-    //     ],
-    //     [
-    //         {
-    //             id: 7,
-    //             number: 1,
-    //             isReserved: true,
-    //             tooltip: "Reserved by Matthias Nadler",
-    //         },
-    //         { id: 8, number: 2, isReserved: true },
-    //         null,
-    //         { id: 9, number: "3", isReserved: true, orientation: "east" },
-    //         { id: 10, number: "4", orientation: "west" },
-    //         null,
-    //         { id: 11, number: 5 },
-    //         { id: 12, number: 6 },
-    //     ],
-    //     [
-    //         { id: 13, number: 1 },
-    //         { id: 14, number: 2 },
-    //         null,
-    //         { id: 15, number: 3, isReserved: true, orientation: "east" },
-    //         { id: 16, number: "4", orientation: "west" },
-    //         null,
-    //         { id: 17, number: 5 },
-    //         { id: 18, number: 6 },
-    //     ],
-    //     [
-    //         { id: 19, number: 1, tooltip: "Cost: 25$" },
-    //         { id: 20, number: 2 },
-    //         null,
-    //         { id: 21, number: 3, orientation: "east" },
-    //         { id: 22, number: "4", orientation: "west" },
-    //         null,
-    //         { id: 23, number: 5 },
-    //         { id: 24, number: 6 },
-    //     ],
-    //     [
-    //         { id: 25, number: 1, isReserved: true },
-    //         { id: 26, number: 2, orientation: "east" },
-    //         null,
-    //         { id: 27, number: "3", isReserved: true },
-    //         { id: 28, number: "4", orientation: "west" },
-    //         null,
-    //         { id: 29, number: 5, tooltip: "Cost: 11$" },
-    //         { id: 30, number: 6, isReserved: true },
-    //     ],
-    // ];
-
-    console.log("Number of remaining Seats is: ", remainingSeats);
-    console.log("seatData is: ", seatData);
-    console.log("ROWS IS: ", rows);
-
     return (
         <div>
-            {/* <h3>{seatData[0].seat_number}</h3> */}
-            {/* <h3>{seatData[0].reserved.toString()}</h3> */}
-            {/* <h1>{rows[0][0].id}</h1> */}
-
             <h1>Seat Picker</h1>
             <div style={{ marginTop: "10px" }}>
                 <SeatPicker
--- frontend/src/pages/seatPicker/SeatPicker.js
+++ frontend/src/pages/seatPicker/SeatPicker.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../../App.css";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
-// import BookingSeat from "../../components/bookingSeat/bookingSeat";
 import BookingSeat from "../../components/bookingSeat/BookingSeat";
+import createRows from "../../components/bookingSeat/createRows";
 function SeatPicker() {
     const history = useHistory();
     const pickSeatsHandler = () => {
         history.chosenSeats = ["E0", "E1", "E7"];
         history.goBack();
     };
+    //this should be passed from booking in previous page
+    //first off we have to check whether requestedSeatsNumber<= remainingSeats to continue picking seats
+    const requestedSeats = 4;
+    const cabinClass = "first";
+    const flightNumber = "TKL-234";
+
+    const [remainingSeats, setRemainingSeats] = useState(-1);
+    const [rows, setRows] = useState([[]]);
+    useEffect(() => {
+        getSeats(flightNumber);
+    }, []);
+
+    const getSeats = async (flightNumber) => {
+        try {
+            const url = `http://localhost:8000/flights/search?flight_number=${flightNumber}`;
+            const response = await axios.get(url);
+            const seatFullData = response.data[0].seats[cabinClass];
+            setRemainingSeats(response.data[0].remaining_seats[cabinClass]);
+            setRows(createRows(seatFullData));
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <>
-            <BookingSeat />
+            <BookingSeat rows={rows} />
 
             <div style={{ textAlign: "center" }}>
                 <button type="button" class="btn-confirm" onClick={pickSeatsHandler}>
