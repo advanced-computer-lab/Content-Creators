14f331dd72170bdd23b971376b4f7f7cc1b90f83
Husseljo <husseljoo@gmail.com>
2021-12-20 14:08:10 +0200

    sign up page state variables in component

--- frontend/src/pages/signUp/SignUpForm.js
+++ frontend/src/pages/signUp/SignUpForm.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import { FlatButton, RaisedButton } from "material-ui";
 import TextField from "material-ui/TextField";
 import PasswordStr from "./PasswordStr";
 import "./style.css";
 import "./index.css";
 
-const SignUpForm = ({ errors, user, score, btnTxt, type }) => {
+const SignUpForm = () => {
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    });
+    const [btnTxt, setBtnTxt] = useState("show");
+    const [score, setScore] = useState(0);
+    const [errors, setErrors] = useState("There is an error!");
+    const [type, setType] = useState("Type whatever");
+
+    console.log("user is: ", user);
     return (
         <div className="loginBox">
             <h1>Sign Up</h1>
@@ -17,12 +29,14 @@ const SignUpForm = ({ errors, user, score, btnTxt, type }) => {
                     floatingLabelText="user name"
                     value={user.username}
                     errorText={errors.username}
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
                 />
                 <TextField
                     name="email"
                     floatingLabelText="email"
                     value={user.email}
                     errorText={errors.email}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
                 />
                 <TextField
                     type={type}
@@ -30,6 +44,7 @@ const SignUpForm = ({ errors, user, score, btnTxt, type }) => {
                     floatingLabelText="password"
                     value={user.password}
                     errorText={errors.password}
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
                 />
 
                 <div className="pwStrRow">
@@ -50,10 +65,15 @@ const SignUpForm = ({ errors, user, score, btnTxt, type }) => {
                 </div>
                 <TextField
                     type={type}
-                    name="pwconfirm"
+                    name="passwordConfirm"
                     floatingLabelText="confirm password"
-                    value={user.pwconfirm}
-                    errorText={errors.pwconfirm}
+                    value={user.passwordConfirm}
+                    errorText={errors.passwordConfirm}
+                    onChange={(e) => {
+                        if (user.password) {
+                            setUser({ ...user, passwordConfirm: e.target.value });
+                        }
+                    }}
                 />
                 <br />
                 <RaisedButton
