0cfb016c8ed803e51971116fc2d97fa4fb342959
Husseljo <husseljoo@gmail.com>
2021-12-24 14:55:52 +0200

    localStorage stores access_token and refresh token on login

--- frontend/src/pages/signIn/SignIn.js
+++ frontend/src/pages/signIn/SignIn.js
@@ -21,20 +21,28 @@ export default function SignIn() {
                 "http://localhost:8000/users/login",
                 user
             );
+            const { username, access_token, refresh_token } = response.data;
             console.log("Response Data is: ", response.data);
             console.log("Response Data Token is: ", response.data.token);
-            if (response.data.token) {
+            if (access_token) {
                 console.log(
                     `successfully logged in user with username ${user.user.username}!`
                 );
+                localStorage.setItem("access_token", access_token);
+                localStorage.setItem("refresh_token", refresh_token);
             } else {
-                setErrorMessage('Please make sure of your username and password combination');
+                setErrorMessage(
+                    "Please make sure of your username and password combination"
+                );
                 console.log("not able to login user!");
+                localStorage.removeItem("access_token");
+                localStorage.removeItem("refresh_token");
             }
             history.push("/");
         } catch (err) {
-            setErrorMessage('wrong username & password combination');
-
+            setErrorMessage("wrong username & password combination");
+            localStorage.removeItem("access_token");
+            localStorage.removeItem("refresh_token");
             console.log(err);
         }
     };
@@ -46,43 +54,43 @@ export default function SignIn() {
     };
     return (
         <div className="SignIn">
-
             <div className="signcontainer">
-                  
-                        <form action="post" className="SignIn.form" onSubmit={handleSubmit}>
-                            <div className="signin-form-group">
-                                <input
-                                    type="text"
-                                    placeholder="Username"
-                                    name="username"
-                                    className="form-control"
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <div className="signin-form-group">
-                                <input
-                                    type="password"
-                                    placeholder="password"
-                                    name="password"
-                                    className="form-control"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            {errorMessage && <div className="signin-form-group"> <p>{errorMessage} </p></div>}
-                            <input type="submit" value="LOG IN" className="button" />
-                            {/* <input type="submit" value="Sign Up" className="button" /> */}
-                            <Link to='/sign-up'>You still have no account?Sign Up</Link>
-                        </form>
-                    
-                </div>
-                
-            
-            <img src="/images/signin.jpg" alt="" height="100%" width="100%" />
+                <form action="post" className="SignIn.form" onSubmit={handleSubmit}>
+                    <div className="signin-form-group">
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                            className="form-control"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="signin-form-group">
+                        <input
+                            type="password"
+                            placeholder="password"
+                            name="password"
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    {errorMessage && (
+                        <div className="signin-form-group">
+                            {" "}
+                            <p>{errorMessage} </p>
+                        </div>
+                    )}
+                    <input type="submit" value="LOG IN" className="button" />
+                    {/* <input type="submit" value="Sign Up" className="button" /> */}
+                    <Link to="/sign-up">You still have no account?Sign Up</Link>
+                </form>
+            </div>
 
+            <img src="/images/signin.jpg" alt="" height="100%" width="100%" />
         </div>
     );
 }
