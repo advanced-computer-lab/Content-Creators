3f4514ba00bc871c02b50c5032d55568e94e52b2
Mohamed Abouras <63314067+Mohamedams@users.noreply.github.com>
2021-12-25 16:36:10 +0200

    aboutus (amongus)

--- frontend/src/pages/aboutUs/AboutUs.js
+++ frontend/src/pages/aboutUs/AboutUs.js
@@ -1,14 +1,13 @@
 import React from 'react'
-
+import "./AboutUs.css";
 export default function AboutUs() {
     return (
-        <div>
-            <h1>About Us</h1>
-            <br />
-            <p>Abbas Ibn Firnas was a great astronomer who built a mechanized planetarium with revolving planets stars, clouds,thunder and lightning produced by hidden mechanisms in the room. He studied mechanical devices and timepieces. </p>
-            <p>He designed a water clock called Al-Maquia, He also devised means of manufacturing glass from sand to produce silica and quartz glass, and he developed a chain of rings that could be used to display the motions of the planets and stars. He also developed a process for cutting rock crystal. Up to then, only the Egyptians knew how to facet crystal. Thereafter Spain no longer needed to export quartz to Egypt, but could finish it at 
-                home. He make an artificial weather simulation room, in which spectators saw stars and clouds, and were astonished by artificial thunder and lightning. 
-                These were due to mechanisms hidden in the basement</p>
+        <div className="aboutUs-container">
+            <div className="ourPhoto2-container"> </div>
+            <div className="ourPhoto4-container"> </div>
+            <div className="ourPhoto1-container"> </div>
+            <div className="ourPhoto3-container"> </div>
         </div>
+       
     )
 }
