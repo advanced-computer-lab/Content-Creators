634255643887288c29676a111b7ecec402eea705
Husseljo <husseljoo@gmail.com>
2021-12-25 14:55:18 +0200

    input handling on sign up better

--- frontend/src/pages/signUp/SignUpForm.js
+++ frontend/src/pages/signUp/SignUpForm.js
@@ -8,142 +8,204 @@ import "../../pages/signUp/signUpForm.css";
 import validation from "./validation";
 import axios from "axios";
 
-
-const SignUpForm = ({submitForm}) => {
+const SignUpForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [first_name, setFirstName] = useState("");
     const [last_name, setLastName] = useState("");
     const [address, setAddress] = useState("");
-    const [country_code, setCountryCode] = useState();
+    const [country_code, setCountryCode] = useState("");
     const [telephone, setTelephone] = useState();
     const [email, setEmail] = useState("");
     const [passport_number, setPassport] = useState("");
-    
+
     const [errors, setErrors] = useState({});
-    const [dataIsCorrect, setDataIsCorrect] = useState(false);
-    let user= {
-        username: String,
-        password: String,
-        first_name: String,
-        last_name: String,
-        address: String,
-        country_code: String,
-        telephone: Number,
-        email: String,
-        passport_number: String,
-    };
-    const handleFormSubmit = (event) =>{
-        event.preventDefault();
-         user = {  username, password,first_name,last_name,address,country_code,telephone,email,passport_number  };
-        setErrors(validation(user));
-        setDataIsCorrect(true);
-        const data ={user:{...user}};
-        axios
-            .post("http://localhost:8000/users/sign-up", data)
-            .then((res) => {
-                this.setState({
-                    username: "",
-                    password: "",
-                });
-                this.props.history.push("/");
-            })
-            .catch((err) => {
-                console.log("CANNOT sign up");
-            });
-    }
+    // const [user, setUser] = useState({
+    //     username: String,
+    //     password: String,
+    //     first_name: String,
+    //     last_name: String,
+    //     address: String,
+    //     country_code: String,
+    //     telephone: String,
+    //     email: String,
+    //     passport_number: String,
+    // });
 
-
-
-    useEffect(() => {
-        if(Object.keys(errors).length===0 && dataIsCorrect){
-            submitForm(true);
+    const createUser = async (user) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:8000/users/sign-up",
+                user
+            );
+            if (response.data.success) {
+                console.log(
+                    `successfully created user with username ${user.user.username}!`
+                );
+            } else {
+                console.log("not able to create user!");
+            }
+        } catch (err) {
+            console.log("error occured while posting the user using axios!");
+            console.log(err);
         }
-    },[errors]);
+    };
 
-    console.log("user is: ", user);
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const user = {
+            username,
+            password,
+            first_name,
+            last_name,
+            address,
+            country_code,
+            telephone,
+            email,
+            passport_number,
+        };
+        const errors = validation(user);
+        setErrors(errors);
+        const data = { user: { ...user } };
+        console.log("SIGN UP ERRORS ARE: ", errors);
+        console.log("SIGN UP USER IS: ", user);
+        if (Object.keys(errors).length == 0) {
+            createUser(data);
+        }
+    };
+
     return (
         <>
-
-<div>
-
-        <div className="container">
-
-            <div className="app-wrapper" >
-
-                <div>
-                    <h2 className="title"> Create Account</h2>
+            <div>
+                <div className="container">
+                    <div className="app-wrapper">
+                        <div>
+                            <h2 className="title"> Create Account</h2>
+                        </div>
+                        <form className="signUpform">
+                            <div className="username">
+                                <label className="label">Username</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                                {errors.username && <p className="error">{errors.username}</p>}
+                            </div>
+                            <div className="password">
+                                <label className="label">Password</label>
+                                <input
+                                    className="input"
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                {errors.password && <p className="error">{errors.password}</p>}
+                            </div>
+                            <div className="firstName">
+                                <label className="label">First Name</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="firstname"
+                                    value={first_name}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                                {errors.first_name && (
+                                    <p className="error">{errors.first_name}</p>
+                                )}
+                            </div>
+                            <div className="lastName">
+                                <label className="label">Last Name</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="lastname"
+                                    value={last_name}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                                {errors.last_name && (
+                                    <p className="error">{errors.last_name}</p>
+                                )}
+                            </div>
+                            <div className="address">
+                                <label className="label">Address</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="address"
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                />
+                                {errors.address && <p className="error">{errors.address}</p>}
+                            </div>
+                            <div className="countryCode">
+                                <label className="label">Country Code</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="countryCode"
+                                    value={country_code}
+                                    onChange={(e) => setCountryCode(e.target.value)}
+                                />
+                                {errors.country_code && (
+                                    <p className="error">{errors.country_code}</p>
+                                )}
+                            </div>
+                            <div className="telephone">
+                                <label className="label">Telephone</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="telephone"
+                                    value={telephone}
+                                    onChange={(e) => setTelephone(e.target.value)}
+                                />
+                                {errors.telephone && (
+                                    <p className="error">{errors.telephone}</p>
+                                )}
+                            </div>
+                            <div className="email">
+                                <label className="label">Email</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                {errors.email && <p className="error">{errors.email}</p>}
+                            </div>
+                            <div className="passportNumber">
+                                <label className="label">Passport Number</label>
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="passportNumber"
+                                    value={passport_number}
+                                    onChange={(e) => setPassport(e.target.value)}
+                                />
+                                {errors.passport_number && (
+                                    <p className="error">{errors.passport_number}</p>
+                                )}
+                            </div>
+                            <div>
+                                <button className="Signupbutton" onClick={handleFormSubmit}>
+                                    Sign up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <form className="signUpform" >
-                    <div className="username">
-                        <label className="label">Username</label>
-                        <input 
-                        className="input" 
-                        type="text" 
-                        name="username" 
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                        />
-                        {errors.username && <p className="error">{errors.username}</p>}
-                    </div>
-                    <div className="password">
-                        <label className="label">Password</label>
-                        <input className="input" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        {errors.password && <p className="error">{errors.password}</p>}
-                    </div>
-                    <div className="firstName">
-                        <label className="label">First Name</label>
-                        <input className="input" type="text" name="firstname" value={first_name} onChange={(e) => setFirstName(e.target.value)}/>
-                        {errors.first_name && <p className="error">{errors.first_name}</p>}
-                    </div>
-                    <div className="lastName">
-                        <label className="label">Last Name</label>
-                        <input className="input" type="text" name="lastname" value={last_name} onChange={(e) => setLastName(e.target.value)}/>
-                        {errors.last_name && <p className="error">{errors.last_name}</p>}
-                    </div>
-                    <div className="address">
-                        <label className="label">Address</label>
-                        <input className="input" type="text" name="address" value={address} onChange={(e) => setAddress(e.target.value)}/>
-                        {errors.address && <p className="error">{errors.address}</p>}
-                    </div>
-                    <div className="countryCode">
-                        <label className="label">Country Code</label>
-                        <input className="input" type="number" name="countryCode" value={country_code} onChange={(e) => setCountryCode(e.target.value)}/>
-                        {errors.country_code && <p className="error">{errors.country_code}</p>}
-                    </div>
-                    <div className="telephone">
-                        <label className="label">Telephone</label>
-                        <input className="input" type="text" name="telephone" value={telephone} onChange={(e) => setTelephone(e.target.value)}/>
-                        {errors.telephone && <p className="error">{errors.telephone}</p>}
-                    </div>
-                    <div className="email">
-                        <label className="label">Email</label>
-                        <input className="input" type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                        {errors.email && <p className="error">{errors.email}</p>}
-                    </div>
-                    <div className="passportNumber">
-                        <label className="label">Passport Number</label>
-                        <input className="input" type="text" name="passportNumber" value={passport_number} onChange={(e) => setPassport(e.target.value)}/>
-                        {errors.passport_number && <p className="error">{errors.passport_number}</p>}
-                    </div>
-                    <div>
-                        <button className="Signupbutton" onClick={handleFormSubmit}>Sign up</button>
-                    </div>
-
-                </form>
             </div>
-
-        </div>
-
-
-        </div>
-        <div className="imagesignup">
-            <img src="/images/signup.jpg" alt="" height="100%" width="100%" />
-
-        </div>
+            <div className="imagesignup">
+                <img src="/images/signup.jpg" alt="" height="100%" width="100%" />
+            </div>
         </>
-    )
-
-    }
+    );
+};
 export default SignUpForm;
--- frontend/src/pages/signUp/validation.js
+++ frontend/src/pages/signUp/validation.js
@@ -1,40 +1,66 @@
-import React from 'react'
+import React from "react";
 import "./SignUpForm";
 
 function validation(user) {
+    let errors = {};
 
-    let errors={};
+    const {
+        username,
+        password,
+        first_name,
+        last_name,
+        address,
+        country_code,
+        telephone,
+        email,
+        passport_number,
+    } = user;
 
-    if(!user.username){
-        errors.username="Username is required.";
+    if (!username) {
+        errors.username = "Username is required.";
     }
-    if(!user.password){
-        errors.password="Password is required.";
-    }else if(user.password.length<8){
-        errors.password="Password must be more than 8 characters.";
+    if (!password) {
+        errors.password = "Password is required.";
+    } else if (password.length < 8) {
+        errors.password = "Password must be more than 8 characters.";
     }
-    if(!user.first_name){
-        errors.first_name="First Name is required.";
+    if (!first_name) {
+        errors.first_name = "First Name is required.";
     }
-    if(!user.last_name){
-        errors.last_name="Last Name is required.";
+    if (!last_name) {
+        errors.last_name = "Last Name is required.";
     }
-    if(!user.address){
-        errors.address="Address is required.";
+    if (!address) {
+        errors.address = "Address is required.";
     }
-    if(!user.country_code){
-        errors.country_code="Country Code is required.";
+    if (!country_code) {
+        errors.country_code = "Country Code is required.";
+    } else {
+        const x = Number(country_code);
+        if (isNaN(x)) {
+            errors.country_code = "Country Code is not a number!";
+        }
     }
-    if(!user.telephone){
-        errors.telephone="Telephone is required.";
+    if (!telephone) {
+        errors.telephone = "Telephone is required.";
+    } else {
+        const x = Number(telephone);
+        if (isNaN(x)) {
+            errors.telephone = "Telephone is not a number!";
+        }
     }
-    if(!user.email){
-        errors.email="Email is required.";
-    }else if(!/\S+@\S+\.\S+/.test(user.email)){
-        errors.email="Email is invalid.";
+    if (!email) {
+        errors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+        errors.email = "Email is invalid.";
     }
-    if(!user.passport_number){
-        errors.passport_number="Passport Number is required.";
+    if (!passport_number) {
+        errors.passport_number = "Passport Number is required.";
+    } else {
+        const x = Number(passport_number);
+        if (isNaN(x)) {
+            errors.passport_number = "Passport Number is not a number!";
+        }
     }
 
     return errors;
