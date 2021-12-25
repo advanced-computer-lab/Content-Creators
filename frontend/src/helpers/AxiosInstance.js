8bfbb43db2fdc31738626748f1e8967f75ce4461
Husseljo <husseljoo@gmail.com>
2021-12-25 22:11:16 +0200

    handled AxiosInterceptor 401 request login redirection

--- frontend/src/helpers/AxiosInstance.js
+++ frontend/src/helpers/AxiosInstance.js
@@ -4,23 +4,8 @@ import { useHistory } from "react-router-dom";
 // Add a request interceptor
 axios.interceptors.request.use(
     (config) => {
-        console.log("INTERCEPTORRRRR IS HERE");
-        console.log("config", config);
-        console.log("config.headers", config.headers);
         const access_token = localStorage.getItem("access_token");
         config.headers.authorization = access_token;
-
-        // if (access_token) {
-        //     // Do something before request is sent
-        //     config.headers.authorization = access_token;
-        //     console.log("config.headers.Authorization", config.headers.authorization);
-        //     // config.baseURL = "https://example.io/api/";
-        //     console.log("ACCESS TOKEN IS:", access_token);
-        // } else {
-        //     window.location.href = "/login";
-        //     console.log("Not authorized BRUV!");
-        //     // history.push("/login");
-        // }
 
         return config;
     },
@@ -32,56 +17,14 @@ axios.interceptors.request.use(
 
 axios.interceptors.response.use(
     (response) => {
-        // block to handle success case
         return response;
     },
     (error) => {
-        console.log("error is:", error);
-        console.log("error response is:", error.response);
-        console.log("error STATUS is:", error.response.status);
+        const status = error.response.status;
         const url = window.location.href.split("/").at(-1);
-        console.log("window.location.href", url);
-        // if (url != "login" ) {
-        //     window.location.href = "/login";
-        // }
-        // window.location.href = "/login";
-        // // block to handle error case
-        // const originalRequest = error.config;
-        // const access_token = localStorage.getItem("access_token");
-        // const refresh_token = localStorage.getItem("refresh_token");
-
-        // if (
-        //     error.response.status === 401 &&
-        //     originalRequest.url === "http://dummydomain.com/auth/token" &&
-        //     !access_token &&
-        //     !refresh_token
-        // ) {
-        //     // Added this condition to avoid infinite loop
-        //     // Redirect to any unauthorised route to avoid infinite loop...
-        //     return Promise.reject(error);
-        // }
-
-        // if (
-        //     error.response.status === 401 &&
-        //     !originalRequest._retry &&
-        //     refresh_token
-        // ) {
-        //     // Code inside this block will refresh the auth token
-
-        //     originalRequest._retry = true;
-        //     return axios
-        //         .post("/auth/token", {
-        //             refresh_token,
-        //         })
-        //         .then((res) => {
-        //             if (res.status === 201) {
-        //                 localStorage.setItem("access_token", res.data.access_token);
-        //                 axios.defaults.headers.common["Authorization"] =
-        //                     "Bearer " + localStorage.getItem("access_token");
-        //                 return axios(originalRequest);
-        //             }
-        //         });
-        // }
+        if (status == 401 && url != "login") {
+            console.log("window.location.href", url);
+        }
         return Promise.reject(error);
     }
 );
